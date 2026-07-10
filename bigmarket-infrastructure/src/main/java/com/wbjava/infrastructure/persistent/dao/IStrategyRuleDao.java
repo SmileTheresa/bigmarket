36601e76d2788e5a58def961b1c90a0e2d355abf
SmileTheresa <341628750@qq.com>
2026-07-10 22:37:50 +0800

    feat:pomm操作

---
+++ bigmarket-infrastructure/src/main/java/com/wbjava/infrastructure/persistent/dao/IStrategyRuleDao.java
@@ -0,0 +1,19 @@
+package com.wbjava.infrastructure.persistent.dao;
+
+import com.wbjava.infrastructure.persistent.po.Strategy;
+import com.wbjava.infrastructure.persistent.po.StrategyRule;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 策略规则 DAO
+ * @create 2023-12-16 13:25
+ */
+@Mapper
+public interface IStrategyRuleDao {
+
+    List<StrategyRule> queryStrategyRuleList();
+
+}
